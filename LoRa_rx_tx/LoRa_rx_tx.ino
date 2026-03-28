75292a4975f1faec0e611dcd7056c805d0804774
esromneb <bjmorse@gmail.com>
2026-03-27 20:35:42 -0700

    faster tx

--- LoRa_rx_tx/LoRa_rx_tx.ino
+++ LoRa_rx_tx/LoRa_rx_tx.ino
@@ -1,17 +1,7 @@
-/**
- * Send and receive LoRa-modulation packets with a sequence number, showing RSSI
- * and SNR for received packets on the little display.
- *
- * Note that while this send and received using LoRa modulation, it does not do
- * LoRaWAN. For that, see the LoRaWAN_TTN example.
- *
- * This works on the stick, but the output on the screen gets cut off.
-*/
-
 
 
 // Turns the 'PRG' button into the power button, long press is off 
-#define HELTEC_POWER_BUTTON   // must be before "#include <heltec_unofficial.h>"
+// #define HELTEC_POWER_BUTTON   // must be before "#include <heltec_unofficial.h>"
 #include <heltec_unofficial.h>
 
 // Pause between transmited packets in seconds.
@@ -43,7 +33,6 @@ volatile bool rxFlag = false;
 long counter = 0;
 uint64_t last_tx = 0;
 uint64_t tx_time;
-uint64_t minimum_pause;
 
 void setup() {
   heltec_setup();
@@ -65,16 +54,12 @@ void setup() {
 }
 
 void loop() {
+  // Serial.println("Loop start");
   heltec_loop();
+  // Serial.println("Loop end");
   
-  bool tx_legal = millis() > last_tx + minimum_pause;
   // Transmit a packet every PAUSE seconds or when the button is pressed
-  if ((PAUSE && tx_legal && millis() - last_tx > (PAUSE * 1000)) || button.isSingleClick()) {
-    // In case of button click, tell user to wait
-    if (!tx_legal) {
-      both.printf("Legal limit, wait %i sec.\n", (int)((minimum_pause - (millis() - last_tx)) / 1000) + 1);
-      return;
-    }
+  if ( (millis() - last_tx > (PAUSE)) ) {
     both.printf("TX [%s] ", String(counter).c_str());
     radio.clearDio1Action();
     heltec_led(50); // 50% brightness is plenty for this LED
@@ -87,11 +72,11 @@ void loop() {
     } else {
       both.printf("fail (%i)\n", _radiolib_status);
     }
-    // Maximum 1% duty cycle
-    minimum_pause = tx_time * 100;
     last_tx = millis();
     radio.setDio1Action(rx);
     RADIOLIB_OR_HALT(radio.startReceive(RADIOLIB_SX126X_RX_TIMEOUT_INF));
+  } else {
+    // 
   }
 
   // If a packet was received, display it and the RSSI and SNR
