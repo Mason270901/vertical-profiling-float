6f3e1f3dbaa85f43d366394fbac9a7dfe1107bab
esromneb <bjmorse@gmail.com>
2026-03-27 19:00:45 -0700

    new lines work

--- LoRa_rx_tx/LoRa_rx_tx.ino
+++ LoRa_rx_tx/LoRa_rx_tx.ino
@@ -52,13 +52,13 @@ void setup() {
   // Set the callback function for received packets
   radio.setDio1Action(rx);
   // Set radio parameters
-  both.printf("Frequency: %.2f MHz\n", FREQUENCY);
+  both.printf("Frequency: %.2f MHz\r\n", FREQUENCY);
   RADIOLIB_OR_HALT(radio.setFrequency(FREQUENCY));
-  both.printf("Bandwidth: %.1f kHz\n", BANDWIDTH);
+  both.printf("Bandwidth: %.1f kHz\r\n", BANDWIDTH);
   RADIOLIB_OR_HALT(radio.setBandwidth(BANDWIDTH));
-  both.printf("Spreading Factor: %i\n", SPREADING_FACTOR);
+  both.printf("Spreading Factor: %i\r\n", SPREADING_FACTOR);
   RADIOLIB_OR_HALT(radio.setSpreadingFactor(SPREADING_FACTOR));
-  both.printf("TX power: %i dBm\n", TRANSMIT_POWER);
+  both.printf("TX power: %i dBm\r\n", TRANSMIT_POWER);
   RADIOLIB_OR_HALT(radio.setOutputPower(TRANSMIT_POWER));
   // Start receiving
   RADIOLIB_OR_HALT(radio.startReceive(RADIOLIB_SX126X_RX_TIMEOUT_INF));
@@ -99,9 +99,9 @@ void loop() {
     rxFlag = false;
     radio.readData(rxdata);
     if (_radiolib_status == RADIOLIB_ERR_NONE) {
-      both.printf("RX [%s]\n", rxdata.c_str());
-      both.printf("  RSSI: %.2f dBm\n", radio.getRSSI());
-      both.printf("  SNR: %.2f dB\n", radio.getSNR());
+      both.printf("RX [%s]\r\n", rxdata.c_str());
+      both.printf("  RSSI: %.2f dBm\r\n", radio.getRSSI());
+      both.printf("  SNR: %.2f dB\r\n", radio.getSNR());
     }
     RADIOLIB_OR_HALT(radio.startReceive(RADIOLIB_SX126X_RX_TIMEOUT_INF));
   }
