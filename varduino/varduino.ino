5a0f45f031631d1f3072d644690c7bf46c4a2649
esromneb <bjmorse@gmail.com>
2026-04-08 01:56:13 -0700

    trying a timing approach

--- varduino/varduino.ino
+++ varduino/varduino.ino
@@ -4,36 +4,53 @@
 // in 1, in 2, in 3, in 4
 // nc  , nc  , blue, orange
 
-#define MOTOR_FEEDBACK 4
-#define HALL_TEST 3
+// #define MOTOR_FEEDBACK 4
+// #define HALL_TEST 3
 
 
 
 // only read while interrupts are disabled
-volatile long isr_pulse_counts = 0;
-volatile bool isr_up = false;
+// volatile long isr_pulse_counts = 0;
+// volatile bool isr_up = false;
 
-volatile unsigned long last_toggle_us = 0;
+// volatile unsigned long last_toggle_us = 0;
 
 // don't print or do any delays in this isr
-void ISR_pulse() {
-  isr_pulse_counts++;
+// void ISR_pulse() {
+//   isr_pulse_counts++;
 
   
-  unsigned long now = micros();
-  // Serial.print("now: ");
-  // Serial.print(now);
-  // Serial.print(" lst: ");
-  // Serial.print(last_toggle_us);
-  // Serial.print(" d: ");
-  // Serial.println( (now - last_toggle_us) );
+//   unsigned long now = micros();
+//   // Serial.print("now: ");
+//   // Serial.print(now);
+//   // Serial.print(" lst: ");
+//   // Serial.print(last_toggle_us);
+//   // Serial.print(" d: ");
+//   // Serial.println( (now - last_toggle_us) );
 
-  if (now - last_toggle_us > 2000) {  // 2ms debounce
-    // if( 1) {
-    last_toggle_us = now;
-    isr_up = !isr_up;
-    digitalWrite(HALL_TEST, isr_up);
-  }
+//   if (now - last_toggle_us > 2000) {  // 2ms debounce
+//     // if( 1) {
+//     last_toggle_us = now;
+//     isr_up = !isr_up;
+//     digitalWrite(HALL_TEST, isr_up);
+//   }
+// }
+
+void waterOut() {
+  // pushes water out
+  digitalWrite(MOTOR_UP, 0);
+  digitalWrite(MOTOR_DOWN, 1);
+}
+
+void waterIn() {
+  // sucks water in 
+  digitalWrite(MOTOR_UP, 1);
+  digitalWrite(MOTOR_DOWN, 0);
+}
+
+void waterStop() {
+  digitalWrite(MOTOR_UP, 0);
+  digitalWrite(MOTOR_DOWN, 0);
 }
 
 
@@ -44,81 +61,120 @@ void setup() {
   pinMode(MOTOR_DOWN, OUTPUT);
 
   // test output to see how hall effect is working
-  pinMode(HALL_TEST, OUTPUT);
+  // pinMode(HALL_TEST, OUTPUT);
 
   // setup feedback
-  pinMode(MOTOR_FEEDBACK, INPUT);
+  // pinMode(MOTOR_FEEDBACK, INPUT);
 
-  attachInterrupt(digitalPinToInterrupt(MOTOR_FEEDBACK), ISR_pulse, RISING);
+  // attachInterrupt(digitalPinToInterrupt(MOTOR_FEEDBACK), ISR_pulse, RISING);
 
 
   // analogWrite(MOTOR_UP, 190);
 
-  // pushes water out
-  // digitalWrite(MOTOR_UP, 0);
-  // digitalWrite(MOTOR_DOWN, 1);
-  // delay(39000-4000);
+  waterOut();
+  delay(39000-4000);
 
   // // takes water in
   // digitalWrite(MOTOR_UP, 1);
   // digitalWrite(MOTOR_DOWN, 0);
   // delay(39000);
 
-  digitalWrite(MOTOR_UP, 0);
-  digitalWrite(MOTOR_DOWN, 0);
+  // digitalWrite(MOTOR_UP, 0);
+  // digitalWrite(MOTOR_DOWN, 0);
+  waterStop();
 }
 
 #define RUN
+
+// positive means we were drawing water
+long delta_water;
+
+// how many us per ml
+// long factor = 130000;
+long factor = 127400;
+
+int waterStatus() {
+  return delta_water / factor;
+}
 
 void loop() {
 
   long count;
 
   #ifdef RUN
-  // takes water in
-  digitalWrite(MOTOR_UP, 1);
-  digitalWrite(MOTOR_DOWN, 0);
+  // // takes water in
+  // digitalWrite(MOTOR_UP, 1);
+  // digitalWrite(MOTOR_DOWN, 0);
 
-  for(int i = 0; i < 39; i++) {
+  // take micros, then call fn immediatly
+  unsigned long now = micros();
+  waterIn();
 
-    // delay(39000);
-    
-    // turn off interrupts
-    // copy out the value
-    // turn them back on
-    noInterrupts();
-    count = isr_pulse_counts;
-    interrupts();
-
+  for(int i = 0; i < 6; i++) {
     Serial.print("i: ");
-    Serial.print(i);
-    Serial.print(" counts: ");
-    Serial.println(count);
-
+    Serial.println(i);
     delay(1000);
   }
+  delay(500);
 
-  digitalWrite(MOTOR_UP, 0);
-  digitalWrite(MOTOR_DOWN, 1);
+  unsigned long then = micros();
+  waterStop();
 
-  for(int i = 0; i < 39; i++) {
+  unsigned long delta = then-now;
 
-    // delay(39000);
+  delta_water += delta;
+
+  char buf[128];
+  snprintf(buf, sizeof(buf), "delta: %lu tot delta: %ld est: %d", delta, delta_water, waterStatus());
+  Serial.println(buf);
+
+  
+
+  delay(4000);
+    // Serial.print(" counts: ");
+    // Serial.println(count);
+
+
+  // for(int i = 0; i < 39; i++) {
+
+  //   // delay(39000);
     
-    // turn off interrupts
-    // copy out the value
-    // turn them back on
-    noInterrupts();
-    count = isr_pulse_counts;
-    interrupts();
+  //   // turn off interrupts
+  //   // copy out the value
+  //   // turn them back on
+  //   noInterrupts();
+  //   count = isr_pulse_counts;
+  //   interrupts();
 
-    Serial.print("i: ");
-    Serial.print(i);
-    Serial.print(" counts: ");
-    Serial.println(count);
+  //   Serial.print("i: ");
+  //   Serial.print(i);
+  //   Serial.print(" counts: ");
+  //   Serial.println(count);
 
-    delay(1000);
-  }
+  //   delay(1000);
+  // }
+
+  // digitalWrite(MOTOR_UP, 0);
+  // digitalWrite(MOTOR_DOWN, 1);
+
+  // for(int i = 0; i < 39; i++) {
+
+  //   // delay(39000);
+    
+  //   // turn off interrupts
+  //   // copy out the value
+  //   // turn them back on
+  //   noInterrupts();
+  //   count = isr_pulse_counts;
+  //   interrupts();
+
+  //   Serial.print("i: ");
+  //   Serial.print(i);
+  //   Serial.print(" counts: ");
+  //   Serial.println(count);
+
+  //   delay(1000);
+  // }
 
 
 #else
